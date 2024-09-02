9dec886bb902f189c8039ebd1f8644e7acb5b182
Pandiaraj-22 <itspandiaraj22@gmail.com>
2024-09-02 10:31:10 +0530

    code .

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { FireFilled} from '@ant-design/icons';
+
+const Logo = () => {
+  return (
+    <div className='Logo'>
+        <div className='logo-icon'>
+            <FireFilled/>
+        </div>
+    </div>
+  )
+}
+
+export default Logo
